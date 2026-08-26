cd88edc807e442ef1895ccb5005188211cd0e026
Dominic Cooper SD2025 <dominic.cooper.2025@uni.strath.ac.uk>
2026-08-26 17:39:06 +0100

    added countdown timers in each booth

--- Assets/Scripts/MainSceneManager.cs
+++ Assets/Scripts/MainSceneManager.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainSceneManager : MonoBehaviour
 {
-
-
     private float delaySeconds = 300f;
+    [SerializeField] private TextMeshProUGUI timerText1;
+    [SerializeField] private TextMeshProUGUI timerText2;
+    [SerializeField] private TextMeshProUGUI timerText3;
+    [SerializeField] private TextMeshProUGUI timerText4;
+    [SerializeField] private TextMeshProUGUI timerText5;
+    [SerializeField] private TextMeshProUGUI timerText6;
+    [SerializeField] private TextMeshProUGUI timerText7;
+    [SerializeField] private TextMeshProUGUI timerText8;
+
+
 
     private float timer;
     private bool hasSwitched;
@@ -20,9 +29,22 @@ public class MainSceneManager : MonoBehaviour
         {
             hasSwitched = true;
             LoadOutcomeScene();
-        }
-    }
+            return;
+        } 
 
+        float timeRemaining = delaySeconds - timer;
+        int minutes = Mathf.FloorToInt(timeRemaining / 60f);
+        int seconds = Mathf.FloorToInt(timeRemaining % 60f);
+        timerText1.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText2.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText3.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText4.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText5.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText6.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText7.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText8.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+    }
 
     private void LoadOutcomeScene()
     {
